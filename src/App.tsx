a4be29ae26ae31f8d8977bc64ee179673764a013
gavin-stork <gstork18@gmail.com>
2026-09-05 01:23:25 -0400

    Add left and right movement controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Board } from './components/Board'
 import { createEmptyBoard } from './game/board'
 import { spawnTile } from './game/spawn';
+import { moveLeft, moveRight, boardsEqual } from './game/moves';
 import './App.css'
 
 function App() {
@@ -10,6 +11,28 @@ function App() {
     const firstSpawn = spawnTile(emptyBoard);
     return spawnTile(firstSpawn)
   });
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowLeft") {
+        const movedBoard = moveLeft(board)
+        if (!boardsEqual(board, movedBoard)) {
+          setBoard(spawnTile(movedBoard))
+        }
+      } else if (event.key === "ArrowRight") {
+        const movedBoard = moveRight(board)
+        if (!boardsEqual(board, movedBoard)) {
+          setBoard(spawnTile(movedBoard))
+        }
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [board]);
 
   return (
     <main>
